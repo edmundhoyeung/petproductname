ab59b8b1b47d7aecb4eabe8131a4f3c1e59de1c5
edmundhoyeung <edmund@petionship.com>
2023-12-20 23:16:19 +0800

    updat eheader

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,12 +16,12 @@ export default function Header() {
       </Link>
       <a
         className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100"
-        href="https://github.com/Nutlope/twitterbio"
+        href="https://plus.petionship.com"
         target="_blank"
         rel="noopener noreferrer"
       >
         <Github />
-        <p>Star on GitHub</p>
+        <p>Go to PetOS</p>
       </a>
     </header>
   );
