f0392c335661d07b16654127ba5af7510a022f47
edmundhoyeung <edmund@petionship.com>
2023-12-21 21:11:26 +0800

    update linkedin url

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
       <Link
-        href="https://www.linkdedin.com/company/petionship"
+        href="https://www.linkedin.com/company/petionship"
         target="_blank"
         rel="noreferrer"
         className="group"
